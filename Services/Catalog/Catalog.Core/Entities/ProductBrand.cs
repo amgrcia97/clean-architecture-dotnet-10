3e7f52bfbc1a853c94654d68aecb21d7d10c2f3f
amgrcia <amgrcia97@gmail.com>
2026-05-01 18:51:01 -0300

    Feat: Add core entities for Catalog microservice including BaseEntity, Product, ProductBrand, and ProductType

---
+++ Services/Catalog/Catalog.Core/Entities/ProductBrand.cs
@@ -0,0 +1,10 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.Core.Entities
+{
+    public class ProductBrand : BaseEntity
+    {
+        [BsonElement("name")]
+        public string Name { get; set; }
+    }
+}
