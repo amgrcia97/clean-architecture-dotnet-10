617679b8351a67224b0f10e794d581a47f949fbf
amgrcia <amgrcia97@gmail.com>
2026-05-01 18:56:02 -0300

    Feat: Add IBrandRepository and ITypeRepository interfaces for product brand and type management

---
+++ Services/Catalog/Catalog.Core/Repositories/IBrandRepsitory.cs
@@ -0,0 +1,10 @@
+﻿using Catalog.Core.Entities;
+
+namespace Catalog.Core.Repositories
+{
+    public interface IBrandRepsitory
+    {
+        Task<IEnumerable<ProductBrand>> GetAllBrands();
+        Task<ProductBrand> GetBrandAsync(int id);
+    }
+}
