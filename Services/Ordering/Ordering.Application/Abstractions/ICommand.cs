d8ab9c8bf3984a9c6bfab102e42dcd29fe377f08
Andrés Murcia García <amgrcia97@gmail.com>
2026-05-21 23:49:24 -0300

    Release: develop → master (#15)
    * feat: implement DiscountRepository and database settings configuration
    
    * feat: discount query, handler, mapper and custom grpc exception
    
    * feat: discount create command & handler
    
    * feat: discount update command & handler
    
    * feat: discount delete command & handler
    
    * feat: discount data base seeder
    
    * feat: create discount service + docker
    
    * feat: enhance documentation with setup, architecture, and contribution guidelines
    
    * Feature: feature/project → develop (#7)
    
    * feat: discount update command & handler
    
    * feat: discount delete command & handler
    
    * feat: discount data base seeder
    
    * feat: create discount service + docker
    
    * feat: enhance documentation with setup, architecture, and contribution guidelines
    
    * test: mail notification
    
    * feat: consuming discount microservice into basket service
    
    * feat: creating ordering project
    
    * feat: creating entities
    
    * feat: creating repositories
    
    * feat: creating db context class
    
    * feat: implementing IAsybc Repository
    
    * feat: implementing order repository
    
    * feat: implementing order dto
    
    * feat: creating cqrs abstraction layer
    
    * Feature: feature/project → develop (#12)
    
    * feat: creating ordering project
    
    * feat: creating entities
    
    * feat: creating repositories
    
    * feat: creating db context class
    
    * feat: implementing IAsybc Repository
    
    * feat: implement command and query handlers for order management with validation

---
+++ Services/Ordering/Ordering.Application/Abstractions/ICommand.cs
@@ -0,0 +1,11 @@
+namespace Ordering.Application.Abstractions;
+
+//Marker Interface not returning result
+public interface ICommand 
+{
+
+}
+//Marker Interface returning result
+public interface ICommand<TResult>
+{
+}
